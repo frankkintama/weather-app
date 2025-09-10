5519cf32c6ca63e539bacdcf5a8152ffa3e5810c
frankkintama <83528715+frankkintama@users.noreply.github.com>
2025-09-10 12:20:19 +0700

    Update useGeolocation.ts

--- src/hooks/useGeolocation.ts
+++ src/hooks/useGeolocation.ts
@@ -1,6 +1,12 @@
 /*Dùng navigator.geolocation để lấy vị trí của người dùng */
 import { useEffect, useState } from "react";
 
+/*hook yêu cầu người dùng cho phép quyền truy cập vị trí hiện tại
+e là đối số(paramenter) dùng trong getCurrentPosition()
+
+onSuccess: nếu lấy vị trí thành công thì dừng lưu, xóa lỗi và 
+gán setData vĩ độ và kinh độ hiện tại
+on Error: nếu lấy vị trí không thành công thì dừng lưu và ghi lại lỗi */
 export default function useGeolocation(){ 
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
