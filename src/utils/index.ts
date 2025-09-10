0274d332a3db0877503c8f1d642914f55b0c2b52
frankkintama <83528715+frankkintama@users.noreply.github.com>
2025-09-10 12:21:01 +0700

    Update index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
+//thời gian đầy đủ
 export const getFormattedDate = (): string => {
     const currentDate = new Date();
     const options: Intl.DateTimeFormatOptions = {
@@ -8,16 +9,17 @@ export const getFormattedDate = (): string => {
         minute: '2-digit',
     };
 
-    return currentDate.toLocaleString('en-US', options);
+    return currentDate.toLocaleString('vi-VN', options);
 };
 
+//thời gian ngắn gọn
 export const getShortDate = (timestamp: number): string => {
-    const date = new Date(timestamp * 1000); 
+    const forecastDate = new Date(timestamp * 1000) /*mặc định xài mili sec, chuyển mili sec sang sec*/; 
     const options: Intl.DateTimeFormatOptions = {
         month: 'short',
         day: 'numeric',
         hour: '2-digit',
     };
 
-    return date.toLocaleString('en-US', options);
+    return forecastDate.toLocaleString('vi-VN', options);
 };
