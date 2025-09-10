5efc5ccd8ac39c95f7c9cdfb2016edba2d7ec84a
frankkintama <83528715+frankkintama@users.noreply.github.com>
2025-09-10 12:20:07 +0700

    Update useFetchWeather.ts

--- src/hooks/useFetchWeather.ts
+++ src/hooks/useFetchWeather.ts
@@ -13,9 +13,9 @@ export default function useFetchWeather(
     const queryEnabled =
     !!searchQuery || (!!geoData?.latitude && !!geoData?.longitude);
 
-    /*queryFn: query function
+    /*queryFn: query function, hàm gọi API
     async bắt đầu khai báo đây là hàm bất đồng bộ 
-    Cách hàm hoạt động: người dùng nhập dữ liệu, dữ liệu sẽ được kiểm tra để trả dữ liệu thời tiết
+    Cách hàm hoạt động: người dùng nhập dữ liệu, dữ liệu sẽ được kiểm tra rồi trả dữ liệu thời tiết (WeatherData)
     quá trình kiểm tra là pending nên ta dùng Promise*/ 
     const queryFn = async (): Promise<WeatherData> => {
 
@@ -39,13 +39,17 @@ export default function useFetchWeather(
     throw new Error("No valid search query or geo location provided.");
     };
 
+    /*useQuery là hook dùng để tự gọi API, lưu cache, quản lí dữ liệu, error, loading trong React
+    cách code hoạt động: khai báo 3 biến theo kết quả useQuery
+    khi thành công trả dữ liệu theo dạng WeatherData, 
+    trả lỗi theo Error, trả loading */
     const {data, error, isLoading} = useQuery<WeatherData, Error>(
         {
             queryKey: ['weather', searchQuery || geoData],
-            queryFn,
-            enabled: queryEnabled,
+            queryFn, //gọi API theo định nghĩa như trên
+            enabled: queryEnabled, //cho phép gọi API theo điều kiện
             staleTime: 60 * 60 * 1000,
-            gcTime: 60 * 60 * 1000
+            gcTime: 60 * 60 * 1000 //60s * 60m * 1000ms = 1hour
         })
 
     return {data, error, isLoading}
